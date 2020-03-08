579d1a047e395b103c0a27683cf7d224cf6a0ad6
Jaya Deb Roy <jaya@Shalinis-Air.home>
2020-03-07 23:12:07 -0500

    First commit

---
+++ Assets/scripts/GenerateCircleMesh.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateCircleMesh : MonoBehaviour {
+	[Range(0,360)]
+	public int span;
+	public Color color;
+	// Use this for initialization
+	void Start () {
+		Vector2 origin = new Vector2 (2, 2);
+		Vector2 dest = new Vector2 (7, 2);
+		GameObject circle = new GameObject ("circle");
+		circle.transform.position = origin;
+		MeshRenderer renderer = circle.AddComponent<MeshRenderer> ();
+		renderer.sharedMaterial = new Material(Shader.Find("UI/Default"));
+		circle.AddComponent<MeshFilter> ();
+		Vector3[] vertices = Vertices (origin, dest);
+		for (int i = 0; i < span*10; i++) {
+			GameObject go = HorizontalMesh.generate (vertices);
+			go.transform.parent = circle.transform;
+			go.transform.RotateAround (origin, Vector3.forward, i/10);
+		}
+		CombineMeshes (circle);
+		renderer.sharedMaterial.color = new Color(color.r, color.g, color.b);
+
+	}
+
+	private void CombineMeshes(GameObject gameObject) {
+		MeshFilter[] meshFilters = gameObject.GetComponentsInChildren<MeshFilter>();
+		CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+		List<GameObject> children = new List<GameObject> ();
+
+		int i = 0;
+		while (i < meshFilters.Length)
+		{
+			combine[i].mesh = meshFilters[i].sharedMesh;
+			combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+			meshFilters[i].gameObject.SetActive(false);
+			children.Add(meshFilters[i].gameObject);
+			i++;
+		}
+		gameObject.transform.GetComponent<MeshFilter>().mesh = new Mesh();
+		gameObject.transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+		gameObject.transform.gameObject.SetActive(true);
+		foreach (Transform child in gameObject.transform) {
+			Destroy (child.gameObject);
+		}
+	}
+	private Vector3[] Vertices (Vector2 origin, Vector2 dest)
+	{
+		float lineBreadth = .1f;
+		Vector3[] vertices = new Vector3[4] {
+			new Vector3 (origin.x, origin.y, 0),
+			new Vector3 (dest.x, dest.y, 0),
+			new Vector3 (origin.x, origin.y + lineBreadth/10f, 0),
+			new Vector3 (dest.x, dest.y + lineBreadth, 0)
+		};
+		return vertices;
+	}
+}
---
+++ Assets/scripts/HorizontalMesh.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HorizontalMesh {
+
+	public static GameObject generate(Vector3[] vertices) {
+		GameObject gameObject = new GameObject ();
+		MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+
+		MeshFilter meshFilter = gameObject.AddComponent<MeshFilter>();	
+		Mesh mesh = new Mesh();
+		//Vector3[] vertices = Vertices(origin, dest);
+
+		mesh.vertices = vertices;
+
+		int[] tris = new int[6]
+		{
+			// lower left triangle
+			0, 2, 1,
+			// upper right triangle
+			2, 3, 1
+		};
+		mesh.triangles = tris;
+
+		Vector3[] normals = new Vector3[4]
+		{
+			-Vector3.forward,
+			-Vector3.forward,
+			-Vector3.forward,
+			-Vector3.forward
+		};
+		mesh.normals = normals;
+
+		Vector2[] uv = new Vector2[4]
+		{
+			new Vector2(0, 0),
+			new Vector2(1, 0),
+			new Vector2(0, 1),
+			new Vector2(1, 1)
+		};
+		mesh.uv = uv;
+
+		meshFilter.mesh = mesh;
+		return gameObject;
+	}
+}
