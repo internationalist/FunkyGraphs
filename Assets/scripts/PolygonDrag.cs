b978997cfe999d798aab97dbb574b15dda8d2c13
Jaya Deb Roy <jaya@Shalinis-Air.home>
2020-03-26 22:09:41 -0400

    Adding scale icons

--- Assets/scripts/PolygonDrag.cs
+++ Assets/scripts/PolygonDrag.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PolygonDrag : MonoBehaviour
 {
     public Texture2D moveTexture;
+    public Texture2D scaleL;
+    public Texture2D scaleR;
+    public Texture2D scaleVert;
+    public Texture2D scaleHoriz;
     public CursorMode cursorMode = CursorMode.Auto;
     public Vector2 hotSpot = Vector2.zero;
+    public enum InteractionType { SCALENW, SCALEN, SCALENE, SCALEE, SCALESE, SCALES, SCALESW, SCALEW, DRAG, ROTATE};
+    public InteractionType interactionType;
 
     Vector3 m3Init;
-    bool drag;
+    bool interaction;
     Vector3 delta;
     bool onTarget; //this will change from a bool to a type to represent drag, scale horiz, scale vertical and scale.
     Transform targetTransform;
@@ -31,35 +37,32 @@ public class PolygonDrag : MonoBehaviour
     {
         if (Input.GetMouseButton(0))
         {
-            if (!drag)
+            if (!interaction)
             {
                 RaycastHit hit;
                 Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
                 if (Physics.Raycast(ray, out hit))
                 {
+                    targetTransform = hit.transform;
                     if (hit.collider.gameObject.CompareTag("Polygon"))
                     {
-                        Cursor.SetCursor(moveTexture, hotSpot, cursorMode);
-                        //onTarget = true;
-                        targetTransform = hit.transform;
+                        interactionType = InteractionType.DRAG;
+                    } else if(hit.collider.gameObject.CompareTag("NW"))
+                    {
+                        interactionType = InteractionType.SCALENW;
                     }
                     delta = (targetTransform.position - m3);
-                    drag = true;
+                    interaction = true;
                 }
-                /*else
-                {
-                    Cursor.SetCursor(null, Vector2.zero, cursorMode);
-                    //onTarget = false;
-                }*/
             }
-            if (drag)
+            if (InteractionType.DRAG.Equals(interactionType))
             {
                 targetTransform.position = delta + m3;
             }
         }
         else
         {
-            drag = false;
+            interaction = false;
             targetTransform = null;
         }
     }
@@ -70,10 +73,45 @@ public class PolygonDrag : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         if (Physics.Raycast(ray, out hit))
         {
-            if("Polygon".Equals(hit.collider.gameObject.tag))
+            switch(hit.collider.gameObject.tag)
+            {
+                case "Polygon":
+                    Cursor.SetCursor(moveTexture, hotSpot, cursorMode);
+                    break;
+                case "NW":
+                    Cursor.SetCursor(scaleL, hotSpot, cursorMode);
+                    break;
+                case "N":
+                    Debug.Log("Mouse over north");
+                    Cursor.SetCursor(scaleVert, hotSpot, cursorMode);
+                    break;
+                case "NE":
+                    Cursor.SetCursor(scaleR, hotSpot, cursorMode);
+                    break;
+                case "E":
+                    Cursor.SetCursor(scaleHoriz, hotSpot, cursorMode);
+                    break;
+                case "SE":
+                    Cursor.SetCursor(scaleL, hotSpot, cursorMode);
+                    break;
+                case "S":
+                    Cursor.SetCursor(scaleVert, hotSpot, cursorMode);
+                    break;
+                case "SW":
+                    Cursor.SetCursor(scaleR, hotSpot, cursorMode);
+                    break;
+                case "W":
+                    Cursor.SetCursor(scaleHoriz, hotSpot, cursorMode);
+                    break;
+            }
+            /*if("Polygon".Equals(hit.collider.gameObject.tag))
             {
                 Cursor.SetCursor(moveTexture, hotSpot, cursorMode);
             }
+            else if ("NW".Equals(hit.collider.gameObject.tag))
+            {
+                Cursor.SetCursor(scaleL, hotSpot, cursorMode);
+            }*/
 
         }
         else
